287d4dee3e41179a8969821d3e14369a9a0dc1d5
tjddnr9553 <tjddnr9553@naver.com>
2024-11-09 23:43:04 +0900

    수정

---
+++ my-portfolio/src/main.tsx
@@ -0,0 +1,12 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <StrictMode>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <App/>
+    </StrictMode>,
+)

49a40783917bc3f0051fabbce1ba2f39571b4034
tjddnr9553 <tjddnr9553@naver.com>
2024-11-10 15:11:12 +0900

    수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '/src/index.css'
+import App from '/src/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-    // eslint-disable-next-line react/react-in-jsx-scope
     <StrictMode>
-        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <App/>
     </StrictMode>,
 )

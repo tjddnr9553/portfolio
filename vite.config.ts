1156052c07a367a21b0bb1d7f4df79773c862064
tjddnr9553 <tjddnr9553@naver.com>
2024-11-10 16:04:26 +0900

    수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import {defineConfig} from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') }

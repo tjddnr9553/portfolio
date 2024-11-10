eefe931d9560440d54c57601f0a5028aed6f08e5
tjddnr9553 <tjddnr9553@naver.com>
2024-11-10 16:05:26 +0900

    수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import {defineConfig} from "vite"
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') }
-    ]
-  }
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "/",
+    define: {
+      "process.env.VITE_DB_URL": JSON.stringify(env.VITE_DB_URL),
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
 
